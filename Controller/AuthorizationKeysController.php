167c8cd932477f2ad088307af18d8ea21fc3a53e
ryu818 <masukawa@nii.ac.jp>
2020-12-15 07:54:16 +0900

    fix: popup型のurlチェックを追加 https://github.com/researchmap/RmNetCommons3/issues/2347#issuecomment-744761847

--- Controller/AuthorizationKeysController.php
+++ Controller/AuthorizationKeysController.php
@@ -20,6 +20,18 @@ App::uses('AppController', 'Controller');
 class AuthorizationKeysController extends AuthorizationKeysAppController {
 
 /**
+ * popup型 チェック用URL
+ * @see https://github.com/researchmap/RmNetCommons3/issues/2347
+ *
+ * @var array
+ */
+	const POPUP_VALIDATE_URLS = [
+		'/cabinets/cabinet_files/download/',
+		'/multidatabases/multidatabase_contents/download/',
+		'/videos/videos/download/',
+	];
+
+/**
  * use components
  *
  * @var array
@@ -90,6 +102,35 @@ class AuthorizationKeysController extends AuthorizationKeysAppController {
 			return;
 		}
 		$url = $this->request->query['url'];
+		$url = $this->__cleansingUrl($url);
 		$this->set('url', $url);
+	}
+
+/**
+ * popup型 URLチェック
+ * @see https://github.com/researchmap/RmNetCommons3/issues/2347
+ *
+ * @param string $url 指定URL
+ * @return string クレンジング後のURL エラーの場合、TopページのURLを返す
+ */
+	private function __cleansingUrl($url): string {
+		$valid = false;
+		foreach (self::POPUP_VALIDATE_URLS as $validateUrl) {
+			if (preg_match('/^' . preg_quote($validateUrl, '/') . '/i', $url)) {
+				$parseUrls = explode('?', $url);
+				if (isset($parseUrls[1])) {
+					// queryサニタイズ
+					$query = str_replace('&amp;', '&', h($parseUrls[1]));
+					$url = $parseUrls[0] . '?' . $query;
+					$valid = true;
+					break;
+				}
+			}
+		}
+		if (!$valid) {
+			// TopページのURLを返す
+			return Configure::read('App.fullBaseUrl');
+		}
+		return $url;
 	}
 }
