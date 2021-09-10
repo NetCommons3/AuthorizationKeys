d1e3544bc8d8d88d6586c906daa288268b97cda8
s-nakajima <nakajimashouhei@gmail.com>
2021-09-10 21:55:46 +0900

    fix: gjslinkエラー修正 https://github.com/edumap-jp/Edumap2/issues/139

--- webroot/js/key_auth_init.js
+++ webroot/js/key_auth_init.js
@@ -10,14 +10,14 @@
  * @param {function($scope)} Controller
  */
 NetCommonsApp.controller('AuthorizationKey',
-['$scope', '$http', 'NC3_URL', function($scope, $http, NC3_URL) {
+    ['$scope', '$http', 'NC3_URL', function($scope, $http, NC3_URL) {
 
-    /**
-     * initialize
-     *
-     * @return {void}
-     */
-    $scope.initialize = function(url) {
-      $http.get(NC3_URL + url);
-    };
-  }]);
+      /**
+       * initialize
+       *
+       * @return {void}
+       */
+      $scope.initialize = function(url) {
+        $http.get(NC3_URL + url);
+      };
+    }]);
