f82d394cc3da980dcfd0dc6b3f5b29c0f8cc167f
s-nakajima <nakajimashouhei@gmail.com>
2021-09-10 21:48:21 +0900

    fix: 登録フォーム（アンケート）の認証キーがウェブアクセラレータに対応していない https://github.com/edumap-jp/Edumap2/issues/139

---
+++ webroot/js/key_auth_init.js
@@ -0,0 +1,23 @@
+/**
+ * Created by AllCreator on 2015/11/06.
+ */
+
+
+/**
+ * LikeSettings Controller Javascript
+ *
+ * @param {string} Controller name
+ * @param {function($scope)} Controller
+ */
+NetCommonsApp.controller('AuthorizationKey',
+['$scope', '$http', 'NC3_URL', function($scope, $http, NC3_URL) {
+
+    /**
+     * initialize
+     *
+     * @return {void}
+     */
+    $scope.initialize = function(url) {
+      $http.get(NC3_URL + url);
+    };
+  }]);
